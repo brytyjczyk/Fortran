ad61e7952cf67bda932f1bac824bd38a4a7f745b
brytyjczyk <32850099+brytyjczyk@users.noreply.github.com>
2019-04-26 20:58:29 +0200

    not working properly

--- main.f90
+++ main.f90
@@ -1,9 +1,9 @@
 program main
 
-  use bettermath
   use naivemath
+  use bettermath
   use dotmath
-
+  
   implicit none
 
   real (kind = 4), allocatable, dimension (:,:) :: A4, B4, C4
@@ -18,6 +18,24 @@ program main
   real :: finish
   
   msize=[10,20,40,80,160,320,640,1280]
+
+  !pliki z wynikami
+  open (unit=1, file = "../res/naive_kind4")
+  open (unit=2, file = "../res/naive_kind8")
+  open (unit=3, file = "../res/naive_kind16")
+  
+  open (unit=4, file = "../res/better_kind4")
+  open (unit=5, file = "../res/better_kind8")
+  open (unit=6, file = "../res/better_kind16")
+  
+  open (unit=7, file = "../res/dot_kind4")
+  open (unit=8, file = "../res/dot_kind8")
+  open (unit=9, file = "../res/dot_kind16")
+  
+  open (unit=10, file = "../res/mat_kind4")
+  open (unit=11, file = "../res/mat_kind8")
+  open (unit=12, file = "../res/mat_kind16")
+  
 
   !alokowanie tablic i tworzenie macierzy
   do i=1,8 
@@ -59,66 +77,100 @@ program main
         end do
      end do
 
-     !naivemull
      
+     !naivemull
      call cpu_time(start)
      C4=naivemull(A4,B4)
      call cpu_time(finish)
 
      write(*,*) "naivemath kind=4",  msize(i), " ", finish-start
+     write(1,*)  msize(i), " ", finish-start
 
      call cpu_time(start)
      C8=naivemull(A8,B8)
      call cpu_time(finish)
 
      write(*,*) "naivemath kind=8",  msize(i), " ", finish-start
+     write(2,*)  msize(i), " ", finish-start
 
      call cpu_time(start)
      C16=naivemull(A16,B16)
      call cpu_time(finish)
 
      write(*,*) "naivemath kind=16",  msize(i), " ", finish-start
+     write(3,*)  msize(i), " ", finish-start
+
      
      !bettermull
-
      call cpu_time(start)
      C4=bettermull(A4,B4)
      call cpu_time(finish)
 
      write(*,*) "bettermath kind=4",  msize(i), " ", finish-start
+     write(4,*)  msize(i), " ", finish-start
 
      call cpu_time(start)
      C8=bettermull(A8,B8)
      call cpu_time(finish)
 
      write(*,*) "bettermath kind=8",  msize(i), " ", finish-start
+     write(5,*)  msize(i), " ", finish-start
 
      call cpu_time(start)
      C16=bettermull(A16,B16)
      call cpu_time(finish)
 
      write(*,*) "bettermath kind=16",  msize(i), " ", finish-start
+     write(6,*)  msize(i), " ", finish-start
 
+     
      !dotmull
-
      call cpu_time(start)
      C4=dotmull(A4,B4)
      call cpu_time(finish)
 
      write(*,*) "dotmath kind=4",  msize(i), " ", finish-start
+     write(7,*)  msize(i), " ", finish-start
 
      call cpu_time(start)
      C8=dotmull(A8,B8)
      call cpu_time(finish)
 
      write(*,*) "dotmath kind=8",  msize(i), " ", finish-start
+     write(8,*)  msize(i), " ", finish-start
 
      call cpu_time(start)
      C16=dotmull(A16,B16)
      call cpu_time(finish)
 
      write(*,*) "dotmath kind=16",  msize(i), " ", finish-start
+     write(9,*)  msize(i), " ", finish-start
 
+     
+     !matmul
+     call cpu_time(start)
+     C4= matmul(A4,B4)
+     call cpu_time(finish)
+
+     write(*,*) "matmul kind=4",  msize(i), " ", finish-start
+     write(10,*)  msize(i), " ", finish-start
+
+     call cpu_time(start)
+     C8= matmul(A8,B8)
+     call cpu_time(finish)
+
+     write(*,*) "matmul kind=8",  msize(i), " ", finish-start
+     write(11,*)  msize(i), " ", finish-start
+
+     call cpu_time(start)
+     C16= matmul(A16,B16)
+     call cpu_time(finish)
+
+     write(*,*) "matmul kind=16",  msize(i), " ", finish-start
+     write(12,*)  msize(i), " ", finish-start
+     
+
+     
      ! dealokowanie tablicy zaalokowaniej
      if(allocated(A4)) deallocate(A4)
      if(allocated(B4)) deallocate(B4)
@@ -133,5 +185,19 @@ program main
      if(allocated(C16)) deallocate(C16)
      
   end do
-    
+
+  close(1)
+  close(2)
+  close(3)
+  close(4)
+  close(5)
+  close(6)
+  close(7)
+  close(8)
+  close(9)
+  close(10)
+  close(11)
+  close(12)
+
+  
 end program main
